1d8312185ef9d42ebc834d64e2a3cffcab5fe41e
Artur <81219764+Mybono@users.noreply.github.com>
2022-11-27 11:31:01 +0300

    Update modalCheck.js

--- modalCheck.js
+++ modalCheck.js
@@ -1,5 +1,5 @@
-// Our story redactor has many different modals, this func test them. We can call it  in test just by: I.modalCheck("create_Placeholder");
-// this function checks: the text, css & buttons by [grabElements](https://github.com/Mybono/CodeCeptJs/blob/main/grabElements.js) func, in each modal, and initiates a visual comparison.
+Our story redactor has many different modals, this func test them. We can call it  in test just by: I.modalCheck("create_Placeholder");
+this function checks: the text, css & buttons by [grabElements](https://github.com/Mybono/CodeCeptJs/blob/main/grabElements.js) func, in each modal, and initiates a visual comparison.
 
 
 async modalCheck(activeModal) {
